9b268a8620fe19af7d3b773da74788980f7e8223
123Fang <38906235+123Fang@users.noreply.github.com>
2024-03-26 17:40:16 +0800

    fix: 修改vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,26 @@ export default defineConfig({
   resolve: {
     extensions: ['.tsx', '.ts', '.vue'],
   },
+  build: {
+    minify: false,
+    lib: {
+      entry: {
+        shared: 'packages/shared/index.ts',
+        core: 'packages/core/index.ts',
+        plugin: 'packages/plugins/auxiliaryLine/index.tsx',
+      },
+      name: 'core-dnd'// window.core-dnd
+    },
+    rollupOptions: {
+      external: 'vue',
+      output: {
+        // 强制 shared 包不打包出单独的 chunk
+        manualChunks(id) {
+          if (id.includes('/packages/shared/')) {
+            return 'shared'
+          }
+        }
+      }
+    }
+  }
 })
